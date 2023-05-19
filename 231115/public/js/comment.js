07e4204479944484864afdc284c74873f889b3b6
chopssal <koalasunghee@gmail.com>
2023-05-19 22:27:44 +0900

    feat: comment issue처리하기

--- 231115/public/js/comment.js
+++ 231115/public/js/comment.js
@@ -1,18 +1,46 @@
-const clickContent = () => {
-    const inputClick = document.querySelector(".commentContent").value=""
-}
+
+const clickContent = document.querySelector('.commentContent').addEventListener("click", () => {
+    const clickInput = document.querySelector(".commentContent").value = ""
+})
+
+
+const commentContent = document.querySelector(".commentContent").addEventListener("submit", e => {
+    // if(commentContent.value == "댓글을 입력해주세요."){
+    //     e.preventDefault()
+    // }else{
+    //     const content = document.querySelector(".commentContent").value
+    //     const setContent = document.querySelector("#addComment").innerText = content
+    // }
+    const content = commentContent.value
+    const setContent = document.querySelector("#addComment").value = content
+})
+
+// const setMouse = () => {
+//     const on = document.querySelector(".commentContent").value
+//     if(on == null || ""){
+//         mouseOver()
+//     }else if(on !== null || "") {
+//         e.preventDefault(() => {
+//             return on
+//         })
+//     }
+// }
+
+// const mouseOver = document.querySelector(".wallpaper").addEventListener("mouseover", e => {
+//     document.querySelector(".commentContent").value = "댓글을 입력해주세요"
+// })
+
 
 let count = 0
 
-// const save = document
-//     .querySelector(".submitBtn")
-//     .addEventListener(
-//         "click", () => {
-//             count = count + 1
-//         })
-
-
-
-    const sub = document.querySelector(".submitWrapper > .submitBtn")
-    console.log("sub", sub)
+const save = document.querySelector(".submitBtn").addEventListener("submit", (e) =>{
+    if(commentContent.value = ""){
+        e.preventDefault((count) => {
+            return count
+        })
+    }else {
+        count = count + 1
+        result.innerText =  "댓글" + " " + count
+    }
+})
 
