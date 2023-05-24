645a625ae201166b75fc7ab20148a42ea714f6d7
chopssal <koalasunghee@gmail.com>
2023-05-25 00:04:03 +0900

    댓글 구현하기 완성

--- 231115/public/js/comment5.js
+++ 231115/public/js/comment5.js
@@ -1,24 +1,21 @@
-// 댓글을 추가하는 함수
-function addComment() {
-    let commentInput = document.querySelector('.commentContent')
-    let commentList = document.getElementById('commentList');
+document.getElementById("commentBtn").addEventListener("click", addComment)
 
-    // 입력된 댓글 내용 가져오기
-    let commentText = commentInput.value;
+function addComment(){
+    // 사용자에게 입력을 받는 공간
+    const userInput = document.getElementById("commentInput")
+    const commentList = document.getElementById("commentList")
+    const commentContent = userInput.value
 
-    // 댓글이 비어있는지 확인
-    if (commentText === '') {
-        alert('댓글을 입력해주세요.');
-        return;
+    if(commentContent == ""){
+        alert("내용을 입력해주세요.")
+    }else{
+        alert("저장되었습니다.")
+        const newComment = document.createElement("div")
+        newComment.innerText = commentContent
+        commentList.appendChild(newComment)
     }
 
-    // 새로운 댓글 요소 생성
-    let newComment = document.createElement('div');
-    newComment.innerText = commentText;
-
-    // 새로운 댓글을 댓글 목록에 추가
-    commentList.appendChild(newComment);
-
-    // 댓글 입력창 초기화
-    commentInput.value = '';
+    userInput.value = ""
 }
+
+
---
+++ 231115_1/public/js/index.js
@@ -0,0 +1,19 @@
+document.getElementById("commentBtn").addEventListener("click", addComment)
+
+function addComment(){
+    let userInput = document.getElementById("commentInput")
+    let commentList = document.getElementById("commentList")
+    let commentContent = userInput.value
+
+    if(commentContent === ""){
+        alert("내용을 입력해주세요.")
+    }else{
+        let newComment = document.createElement("div")
+        newComment.innerText = commentContent
+    
+        commentList.appendChild(newComment)
+        userInput.value = ""
+    }
+}
+
+
