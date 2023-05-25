ca25cebf310898b97bb48d789a610fe557c529d3
chopssal <koalasunghee@gmail.com>
2023-05-25 09:28:23 +0900

    moment설치

--- 231115_1/public/js/index.js
+++ 231115_1/public/js/index.js
@@ -3,7 +3,7 @@ document.getElementById("commentBtn").addEventListener("click", addComment)
 function addComment(){
     let userInput = document.getElementById("commentInput")
     let commentList = document.getElementById("commentList")
-    let commentContent = userInput.value
+    let commentContent = "" + userInput.value + ""
 
     if(commentContent === ""){
         alert("내용을 입력해주세요.")
